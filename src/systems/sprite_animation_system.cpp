ff440e8049b4b0d4df9aa6f2262247021c4d7cad
Seth N. Hetu <seth.hetu@gmail.com>
2019-04-18 19:00:51 -0400

    Some sequence work; however, there is a weird artifact that seems unresolvable.

--- src/systems/sprite_animation_system.cpp
+++ src/systems/sprite_animation_system.cpp
@@ -9,7 +9,17 @@ public:
 		auto vel = mainFamily.mob.faceDir;
 		int dir = std::abs(vel.y) > std::abs(vel.x) ? (vel.y < 0 ? 0 : 2) : (vel.x < 0 ? 3 : 1);
 
+		//setSequence
+
 		auto& player = mainFamily.spriteAnimation.player;
+
+		// Are we moving?
+		if (mainFamily.mob.moveDir.squaredLength() > 0.5f) {
+			player.setSequence("walking");
+		} else {
+			player.setSequence("idle");
+		}
+
 		player.setDirection(dir);
 		player.update(time);
 		player.updateSprite(sprite);
